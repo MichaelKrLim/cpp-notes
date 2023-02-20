308d81ad5e07fd2cb15c3466e0c14aef53611aed
Michael Lim <83725834+MichaelKrLim@users.noreply.github.com>
2023-02-20 19:11:58 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -33,3 +33,28 @@ std::copy(answer.begin(),answer.end(),std::ostream_iterator<join>{std::cout});
 accumulate can sum values from two iterators, the type is determined based on the last parameter of accumulate  
                                                                                   vv
 long long sum_e = std::accumulate(std::istream_iterator<long long> {stream}, {}, 0ll);
+
+---------------------------------------------------------------------
+   
+conversion between bases:
+
+
+digits = ////
+   
+int base = out == "dec" ? 10 : out == "hex" ? 16 : 2;
+            std::string output;
+            do 
+            { 
+                output += digits[dec % base];
+                dec /= base;
+            } while (dec > 0);
+
+
+
+output must be big enough to store converted value:
+
+
+output.resize(1000);
+std::to_chars(&output[0], &output[output.size()], dec, base);
+
+----------------------------------------------------------------------
